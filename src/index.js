77ad0320a30747ca7d1b1cdd45f2146342ce2404
Thomas <tombutler4@hotmail.com>
2023-05-15 21:38:07 -0500

    changed homepage and removed strict mode

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <HashRouter>
         <App />
     </HashRouter>
-  </React.StrictMode>
 );
