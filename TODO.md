c5851ea69c82f73b1840ab270fea34ecec138e2e
Thomas <tombutler4@hotmail.com>
2023-06-04 01:35:54 -0500

    basic post preview linking

---
+++ TODO.md
@@ -0,0 +1,11 @@
+
+#TODO
+- functionality to create a post
+    - load from db?
+    - store markdown files on ipfs?
+    - link post preview to post page 
+
+- post comments 
+    - create comment box component 
+    - post comments with connected wallet as username 
+    - support ens names on comments 
--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -1,25 +1,28 @@
 
 import {
     Box,
-    Text
+    Text,
+    LinkBox,
+    LinkOverlay
 } from '@chakra-ui/react';
 
 function PostPreview() {
     return (
-        <Box m={10} background="gray">
+        <LinkBox m={10} background="gray">
+            <LinkOverlay href='#/post'>
+                <Box display="flex">
+                    <Text>Post Title</Text>
+                    <Text marginLeft="auto">Post Date</Text>
+                </Box>
 
-            <Box display="flex">
-                <Text>Post Title</Text>
-                <Text marginLeft="auto">Post Date</Text>
-            </Box>
-
-            <Box p={10}>
-                <Text>
-                    This is going to be a preview of the blog post contents.
-                    It will be a short preview of the contents of the blog post.
-                </Text>
-            </Box>
-        </Box>
+                <Box p={10}>
+                    <Text>
+                        This is going to be a preview of the blog post contents.
+                        It will be a short preview of the contents of the blog post.
+                    </Text>
+                </Box>
+            </LinkOverlay>
+        </LinkBox>
     );
 }
 
