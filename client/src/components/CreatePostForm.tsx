c6484060f6c11015e2302cf705e79b80e73a8f80
Thomas <tombutler4@hotmail.com>
2023-06-08 17:41:00 -0500

    created post upload page

--- client/src/components/CreatePostForm.tsx
+++ client/src/components/CreatePostForm.tsx
@@ -1,12 +1,41 @@
 import {
-    Box
+    Box,
+    Text,
+    Button,
+    Input 
 } from '@chakra-ui/react'
+import React, { useState } from 'react';
 
 
 function CreatePostForm() {
+    const [title, setTitle] = useState("")
+    const [cid, setCid] = useState("")
+
+    function uploadPost(title: string, cid: string) {
+        // TODO: call smart contract function with post title and cid
+        console.log(`title: ${title}`)
+        console.log(`cid: ${cid}`)
+    }
+
     return (
-        <Box>
-            
+        <Box
+            padding={20}
+            justifyContent="center"
+            m={40}
+        >
+            <Input 
+                placeholder='Post Title' 
+                size='lg'
+                onChange={(e) => setTitle(e.target.value)}
+            /> 
+            <Input 
+                placeholder='CID' 
+                size='lg' 
+                onChange={(e) => setCid(e.target.value)}
+            /> 
+            <Button
+                onClick={() => uploadPost(title, cid)} 
+            >Upload</Button>
         </Box>
     )
 }
