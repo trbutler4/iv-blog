4014e42464d7bbc13113839fda58e26f5c37c21e
Thomas Butler <trbiv@proton.me>
2026-06-06 23:49:06 -0500

    simple deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+VPS_USER="trbiv"
+VPS_HOST="157.230.84.210"
+DEPLOY_DIR="/home/trbiv/iv-blog"
+
+echo "Building..."
+yarn build
+
+echo "Deploying to $VPS_USER@$VPS_HOST:$DEPLOY_DIR..."
+scp -r build/* "$VPS_USER@$VPS_HOST:$DEPLOY_DIR/"
+
+echo "Done."
